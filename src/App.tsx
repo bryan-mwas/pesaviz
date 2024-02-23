4cb50b0b27aac1468d4da7fbf3f4e59e86f679a0
Bryan Mwas <brian.wangome@bosch.com>
2024-02-23 09:08:37 +0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <Container fluid className="text-center">
+      Hello, World.
+    </Container>
+  );
+}
+
+export default App;
